518b0033441117729f29e78d694aa890967ba0b2
Erick Ogaro <ogaroerick1998@gmail.com>
2022-04-27 05:09:43 +0300

    print statements language update

--- replicate_tables.py
+++ replicate_tables.py
@@ -1,7 +1,7 @@
 import camelot
 import os
 
-print("Extracting tables, please wait...")
+print("Extracting tables from PDF, please wait...")
 
 # path of source pdf
 source_file = "./source/data-new.pdf"
@@ -14,11 +14,11 @@ if not os.path.isdir(output_folder):
 # extract all the tables in the PDF file
 tables = camelot.read_pdf(source_file, pages="all")
 
-#  export tables to Excel - same workbook but each table in its own worksheet
+# export tables to Excel - same workbook but each table in its own worksheet
 try:
     tables.export(os.path.join(output_folder, f"exported_tables.xlsx"), f='excel', compress=False)
 except:
-    print("Unable to convert to Excel")
+    print("Unable to convert PDF tables to Excel. Please make sure you have a valid PDF file.")
 
 # number of tables extracted
-print("Total tables extracted:", tables.n)
+print("Total number of tables extracted:", tables.n)
